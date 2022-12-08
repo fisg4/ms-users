c23bbca8b0258917c0ac0eef7db7a4a7ce5ae641
joszamama <joszamama@gmail.com>
2022-12-08 21:27:22 +0100

    create user encrypts pass, still working on update user to encrypt too

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs")
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -29,4 +30,50 @@ userSchema.methods.cleanup = function() {
     }
 }
 
+userSchema.pre("save", function (next) {
+    const user = this
+  
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
+
+userSchema.pre("updateOne", function (next) {
+    const user = this
+
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
+  
 module.exports = mongoose.model('User', userSchema);
