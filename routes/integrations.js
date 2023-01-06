f240eef94ffeed33b577af64d90fb3dba8bd16a2
carnuare <carnuare@gmail.com>
2023-01-06 19:15:48 +0100

    Add jwt

--- routes/integrations.js
+++ routes/integrations.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const passport = require('passport');
 
 const songs_url_base = (process.env.SONGS_HOST);
 
@@ -7,11 +8,33 @@ const songs_url_base = (process.env.SONGS_HOST);
 
 router.post('/', async (req, res) => {
     try {
-        const song = await axios.post(songs_url_base + 'api/v1/likes', {
-            songId: req.body.songId,
-            userId: req.body.userId
-        });
-        res.status(201).json(song);
+        passport.authenticate("jwt", { session: false }),
+        fetch(songs_url_base + 'api/v1/likes', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + req.headers.authorization.split(' ')[1]
+            },
+            body: JSON.stringify({
+              songId: req.body.songId,
+              userId: req.body.userId
+            })
+          })
+          .then(response => {
+            if (response.status === 201) {
+              return response.json();
+            } else if (response.status === 409) {
+              throw new Error('Conflict: Duplicate');
+            } else {
+              throw new Error('Unexpected response');
+            }
+          })
+          .then(song => {
+            res.status(201).json(song);
+          })
+          .catch(error => {
+            res.status(error.response ? error.response.status : 500).json({ message: error.message });
+          });          
     } catch (err) {
         res.json({
             message: err
@@ -22,12 +45,30 @@ router.post('/', async (req, res) => {
 // GET all songs liked from a user
 router.get('/all', async (req, res) => {
     try {
-        const song = await axios.get(songs_url_base + 'api/v1/likes', {
+        fetch(songs_url_base + 'api/v1/likes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             params: {
                 userId: req.query.userId
             }
+        })
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 204) {
+                throw new Error('No content');
+            }else  {
+                throw new Error('Unexpected response');
+            }
+        })
+        .then(songs => {
+            res.status(200).json(songs);
+        })
+        .catch(error => {
+            res.status(error.response ? error.response.status : 500).json({ message: error.message });
         });
-        res.status(200).json(song);
     } catch (err) {
         res.json({
             message: err
