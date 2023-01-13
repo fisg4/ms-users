1bd8cbbaead1ec782b4f70ea31ae756d41c456ea
joszamama <joszamama@gmail.com>
2023-01-13 19:14:50 +0100

    feat: using api for bad word detection

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const bcrypt = require("bcryptjs")
+const axios = require("axios");
 
 const User = require('../models/User');
+const { json } = require('body-parser');
+
+const badwordfilter = 'https://community-purgomalum.p.rapidapi.com/json';
+const xrapidapihost = 'community-purgomalum.p.rapidapi.com';
+const xrapidkey = '4ac446483cmsh72be9fe1b3a03f1p15c2dejsn63485dda4b87';
 
 router.get('/', async (req, res) => {
 
@@ -29,28 +35,57 @@ router.get('/:UserId', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    email = req.body.email;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: 'Email already in use' });
-    } else {
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            role: req.body.role,
-            plan: req.body.plan 
-        });
-        
-        try {
-            const savedUser = await user.save();
-            res.status(201).json(savedUser);
-        } catch (err) {
-            res.json({
-                message: err
-            });
-        }
+
+    try {
+
+        const options = {
+            method: 'GET',
+            url: badwordfilter,
+            params: {text: "" + req.body.username + ", " + req.body.email + ""},
+            headers: {
+              'X-RapidAPI-Key': xrapidkey,
+              'X-RapidAPI-Host': xrapidapihost
+            }
+          };
+          
+          axios.request(options).then(async function (response) {
+              if (JSON.stringify(response.data).includes("*")) {
+                    console.log("Bad words detected");
+                    res.status(401).json({ error: 'Bad words detected' });
+              } else {
+                email = req.body.email;
+                const existingUser = await User.findOne({ email });
+                if (existingUser) {
+                    return res.status(400).json({ error: 'Email already in use' });
+                } else {
+                    const user = new User({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: req.body.password,
+                        role: req.body.role,
+                        plan: req.body.plan 
+                    });
+                    
+                    try {
+                        const savedUser = await user.save();
+                        res.status(201).json(savedUser);
+                    } catch (err) {
+                        res.json({
+                            message: err
+                        });
+                    }
+                }
+            }
+          }).catch(function (error) {
+              console.error(error);
+          });
+    
+
+    } catch (err) {
+        res.status(400).json({ error: 'Quota reached' });
     }
+
+
 });
 
 router.put('/:UserId', async (req, res) => {
